03a3ff2b3a9c2796ee7bd96d145e11e555d6812f
Smiley <40720795+ismailtkngr@users.noreply.github.com>
2024-03-12 18:56:01 +0300

    test_project.py

--- test_project.py
+++ test_project.py
@@ -1,28 +1,41 @@
 import unittest
 from unittest.mock import patch
 from io import StringIO
-import project.py
+from project import snake_game, main 
 
-class TestProject(unittest.TestCase):
-
-    def setUp(self):
-        self.held_output = StringIO()
-        sys.stdout = self.held_output
-
-    def tearDown(self):
-        sys.stdout = sys.__stdout__
+class TestSnakeGame(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['q'])
     def test_game_exit_on_q(self, mock_input):
-        project.snake_game()
-        output = self.held_output.getvalue().strip()
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main()  
+
+        output = mock_stdout.getvalue().strip()
         self.assertTrue("Game Over" in output)
 
     @patch('builtins.input', side_effect=['Y', 'q'])
     def test_play_again(self, mock_input):
-        project.snake_game()
-        output = self.held_output.getvalue().strip()
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main() 
+
+        output = mock_stdout.getvalue().strip()
         self.assertTrue("Do you want to play again?" in output)
+        
+    @patch('builtins.input', side_effect=['Y', 'Y', 'Y', 'q'])
+    def test_multiple_play_again(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main() 
+
+        output = mock_stdout.getvalue().strip()
+        self.assertTrue("Do you want to play again?" in output)
+
+    @patch('builtins.input', side_effect=['a', 'q'])
+    def test_invalid_key(self, mock_input):
+        with patch('sys.stdout', new_callable=StringIO) as mock_stdout:
+            main()  
+
+        output = mock_stdout.getvalue().strip()
+        self.assertTrue("Game Over" in output)
 
 
 if __name__ == '__main__':
